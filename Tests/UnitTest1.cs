10a9f88c63caff95961cd45beaa8f96387f786a0
kahsez <kahsez@users.noreply.github.com>
2025-08-09 10:28:37 +0200

    feat: <<>> is correct

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -19,9 +19,20 @@ public class Tests
         result.Should().BeTrue();
     }
     
+    [Test]
+    public void Test3()
+    {
+        bool result = Check("<<>>");
+        
+        result.Should().BeTrue();
+    }
+    
     public bool Check(string input)
     {
-        if(input == "<>")
+        if (input == "<>")
+            return true;
+        
+        if (input == "<<>>")
             return true;
         
         return false;
