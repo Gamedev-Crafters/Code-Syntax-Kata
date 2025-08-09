5699c58a882574d860c899a854a708cb33690440
Samuel de los Reyes <samueldelosreyes@odderslab.com>
2025-08-09 10:18:49 +0200

    feat: < is not correct

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,11 +1,18 @@
+using FluentAssertions;
 namespace Tests;
 
-public class Tests {
-	[SetUp]
-	public void Setup() { }
-
-	[Test]
-	public void Test1() {
-		Assert.Pass();
-	}
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        bool result = Check("<");
+        
+        result.Should().BeFalse();
+    }
+    
+    public bool Check(string input)
+    {
+        return false;
+    }
 }
